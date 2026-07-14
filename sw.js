216ddc646010fd19a290a036b6c6710a53261beb
naganagaa <47709064+naganagaa@users.noreply.github.com>
2026-07-14 09:57:03 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'paisa-v14';
+const CACHE_VERSION = 'paisa-v15';
 const ASSETS = ['/paisa-app/','/paisa-app/index.html','/paisa-app/manifest.json','/paisa-app/icon-192.png','/paisa-app/icon-512.png'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_VERSION).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())); });
 self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE_VERSION).map(k => caches.delete(k)))).then(() => self.clients.claim())); });
